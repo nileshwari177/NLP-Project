33d647c64eae2fb74e6d518d77f87b2e2374b0ec
nileshwari177 <gawandenn@rknec.edu>
2026-02-04 14:29:30 +0530

    Schema for Testing

---
+++ schema_registry.py
@@ -0,0 +1,20 @@
+SCHEMA = {
+    "students": {
+        "columns": {
+            "id": "integer",
+            "name": "string",
+            "cgpa": "float",
+            "marks": "integer",
+            "year": "integer"
+        }
+    },
+    "cars": {
+        "columns": {
+            "id": "integer",
+            "car_name": "string",
+            "price": "float",
+            "units_sold": "integer",
+            "year": "integer"
+        }
+    }
+}
